df0475741f8d42cec6657dc9679ee8af2ba41d38
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-09-03 17:58:56 -0700

    Added view models for restaurant and creator + mapper

---
+++ webapi/Data/Configurations/MapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using webapi.Models;
+
+namespace webapi.Data.Configurations
+{
+    public class MapperConfig: Profile
+    {
+        public MapperConfig()
+        {
+            //CreateMap<RestaurantModel, RestaurantVM>().ReverseMap();
+            CreateMap<UserModel, UserVM>().ReverseMap();
+            CreateMap<UserModel, CreatorVM>().ReverseMap();
+            CreateMap<RestaurantModel, RestaurantVM>()
+                .ForMember(dest => dest.Creator, opt => opt.MapFrom(src => src.Creator));
+
+        }
+    }
+}
---
+++ webapi/Models/CreatorVM.cs
@@ -0,0 +1,15 @@
+﻿namespace webapi.Models
+{
+    public class CreatorVM
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public List<string>? FavouriteStyles { get; set; }
+        public int? BudgetMin { get; set; }
+        public int? BudgetMax { get; set; }
+        public string? Picture { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateJoined { get; set; }
+    }
+}
