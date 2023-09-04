dad6c598df92edee8941646021778605ee95fab9
maxime m3.code <maxime.marechalmccoy@gmail.com>
2023-09-03 18:24:44 -0700

    Implemented Generic Services

---
+++ webapi/Interfaces/IGenericService.cs
@@ -0,0 +1,15 @@
+﻿namespace webapi.Interfaces
+{
+    public interface IGenericService<T> where T : class
+    {
+        Task<T> GetAsync(int? id);
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task<int> GetCountAsync();
+        Task<bool> Exists(int id);
+        Task<T> AddAsync(T entity);
+        Task AddRangeAsync(List<T> entities);
+        Task DeleteAsync(int id);
+        Task UpdateAsync(T entity);
+    }
+}
